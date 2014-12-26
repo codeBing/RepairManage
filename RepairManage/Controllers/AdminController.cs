afeb94cf109a5fbaf365dce224e1e3d657caccec
BING <407272759@qq.com>
2014-12-26 20:56:34 +0800

    Revert "Revert "repairManage first commit""
    This reverts commit bd2594b8a4b019f13f4b6bcb887840b732ea589e.

---
+++ RepairManage/Controllers/AdminController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RepairManage.Models;
+using System.Web.Security;
+
+namespace RepairManage.Controllers
+{ 
+    public class AdminController : Controller
+    {
+        private RepairSystemContext db = new RepairSystemContext();
+
+        //
+        // GET: /Admin/LogOn
+
+        public ActionResult LogOn()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Admin/LogOn
+
+        [HttpPost]
+        public ActionResult LogOn(Administrator model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ValidateUser(model))
+                {
+
+                    FormsAuthentication.SetAuthCookie(model.adminName, true);
+                    return RedirectToAction("Home", "Admin");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "用户名或密码不正确！");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        //
+        // GET: /Account/LogOff
+
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+
+            return RedirectToAction("LogOn", "Admin");
+        }
+
+        private bool ValidateUser(Administrator model)
+        {
+            string name = model.adminName;
+            string pwd = model.adminPwd;
+            var count = db.Administrator.Where(admin => admin.adminName == name && admin.adminPwd == pwd);
+            if (count.Count() == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        [Authorize]
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
---
+++ RepairManage/Models/Computer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace RepairManage.Models
+{
+    public class Computer
+    {
+        
+        //public int ID { get; set; }
+        [Key]
+        [Display(Name = "序列号")]
+        public int computerId { get; set; }
+        [ScaffoldColumn(false)]
+        public int centerId { get; set; }
+        [Display(Name = "过保日期")]
+        [DataType(DataType.Date)]
+        public DateTime deadTime { get; set; }
+
+        public virtual Center center { get; set; }
+    }
+}
